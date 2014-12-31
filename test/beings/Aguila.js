5301c65055cf75f3c8cfa327191cb6e7c45351c9
facundopereztomasek <facundopereztomasek@gmail.com>
2014-12-31 04:17:20 -0300

    El mejor aguila

---
+++ test/beings/Aguila.js
@@ -0,0 +1,17 @@
+function Aguila( params ){
+	extend( this , Being , params );
+
+	var defaults = {
+
+		/*Nombre del elfo*/
+		"name":"Aguila",
+		"specie":"Aguila"
+	}
+
+	/*Mezcla de los defaults con los parametros pasados al objeto*/
+	merge( defaults , params , this );
+
+	// Se agrega en el mapa
+	this.world.getTile( this.position ).append({ 'being' : this });
+	this.createControls();
+}
